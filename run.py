32547f4b25c06baf8e5df672e1707b533410fb82
kye22 <nano.lopezmoreno@gmail.com>
2025-03-31 09:49:23 +0200

    chat.html

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 from services import get_response
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/frontend/templates")
 app.config.update(
     SECRET_KEY=os.environ.get('APP_SECRET_KEY'),
 )
@@ -15,6 +15,10 @@ CORS(app)
 @app.route("/")
 def home():
     return render_template("layout.html")
+
+@app.route("/chat")
+def chat():
+    return render_template("chat.html")
 
 
 @app.route("/completion", methods=["POST"])
