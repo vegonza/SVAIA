564e5b77af96e62812bebedef6398df7806f5fc7
Gonzalo Ramos <vegonza3@gmail.com>
2025-03-31 09:32:21 +0200

    changed the structure of the app

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 from services import get_response
 
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+app = Flask(__name__, template_folder="../frontend")
+CORS(app)
+
+
+@app.route("/")
+def home():
+    return render_template("/home/index.html")
 
 
 @app.route("/completion", methods=["POST"])
