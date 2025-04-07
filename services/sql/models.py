013daf2e88ecc561b78993d0729cb1468a27bdef
kye22 <nano.lopezmoreno@gmail.com>
2025-04-07 10:20:44 +0200

    models

--- services/sql/models.py
+++ services/sql/models.py
@@ -1,3 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+app = Flask(__name__)
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(app, model_class=Base)
+
+class Project(db.Model):
+    """Table for storing project information."""
+    uuid: Mapped[str] = mapped_column(db.String(36), primary_key=True, nullable= False)
+    name: Mapped[str] = mapped_column(db.String(50), nullable=False)
+
+class User(db.Model):
+    """Table for storing user information."""
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(db.String(12), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(db.String(12), nullable=False)
+    projects_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey('project.uuid'))
+    
+
+with app.app_context():
+    db.create_all()
