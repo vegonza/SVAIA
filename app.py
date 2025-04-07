e6d5709c928ef0d957a7b9c0afe03118334992e5
jorvarea <jorvarea@users.noreply.github.com>
2025-04-07 10:10:08 +0200

    pusheado sin errores

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 
 from flask import Flask, render_template, send_from_directory
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required
+from services.test_users import users
+
 from flask_cors import CORS
 from services import auth_bp, chat_bp
 
@@ -12,7 +14,7 @@ app.config.update(
 CORS(app)
 
 login_manager = LoginManager()
-login_manager.init(app)
+login_manager.init_app(app)
 login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Debes iniciar sesión para acceder a esta página.'
 login_manager.login_message_category = 'info'
