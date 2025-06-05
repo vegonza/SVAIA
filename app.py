5bf5e10fcf3775e015456841ffe565adde9e1439
jorvarea <jorvarea@users.noreply.github.com>
2025-06-05 03:07:51 +0200

    remove debug mode for production deployment

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ app.register_blueprint(sql_bp, url_prefix="/sql")
 app.register_blueprint(admin_bp, url_prefix="/admin")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
